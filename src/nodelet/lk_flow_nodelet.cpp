7493ecded7b3443444c05d3c0a3b074ad9094cfe
Kei Okada <kei.okada@gmail.com>
2016-08-24 04:51:01 +0000

    lk_flow : need to explicitly include sensor_msgs/image_endcodings.h

--- src/nodelet/lk_flow_nodelet.cpp
+++ src/nodelet/lk_flow_nodelet.cpp
@@ -40,6 +40,7 @@
 #include <ros/ros.h>
 #include "opencv_apps/nodelet.h"
 #include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
 #include <cv_bridge/cv_bridge.h>
 
 #include <opencv2/highgui/highgui.hpp>
