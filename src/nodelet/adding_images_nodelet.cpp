82ff0e29d875bdb47a674b6ba60cee8665f8bb37
iory <ab.ioryz@gmail.com>
2016-08-31 14:04:13 +0900

    [adding_images] Arranged nodelet same as other nodelet

--- src/nodelet/adding_images_nodelet.cpp
+++ src/nodelet/adding_images_nodelet.cpp
@@ -41,6 +41,7 @@
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <dynamic_reconfigure/server.h>
 
 #include <image_transport/image_transport.h>
@@ -60,6 +61,10 @@ namespace adding_images {
     boost::shared_ptr<image_transport::ImageTransport> it_;
 
     typedef message_filters::sync_policies::ExactTime<
+      sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo> SyncPolicyWithCameraInfo;
+    typedef message_filters::sync_policies::ApproximateTime<
+      sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo> ApproxSyncPolicyWithCameraInfo;
+    typedef message_filters::sync_policies::ExactTime<
       sensor_msgs::Image, sensor_msgs::Image> SyncPolicy;
     typedef message_filters::sync_policies::ApproximateTime<
       sensor_msgs::Image, sensor_msgs::Image> ApproxSyncPolicy;
@@ -72,8 +77,16 @@ namespace adding_images {
     Config config_;
     boost::shared_ptr<ReconfigureServer> reconfigure_server_;
 
+    bool debug_view_;
+    ros::Time prev_stamp_;
+
+    std::string window_name_;
+
     image_transport::SubscriberFilter sub_image1_, sub_image2_;
     image_transport::Publisher img_pub_;
+    message_filters::Subscriber<sensor_msgs::CameraInfo> sub_camera_info_;
+    boost::shared_ptr<message_filters::Synchronizer<SyncPolicyWithCameraInfo> > sync_with_info_;
+    boost::shared_ptr<message_filters::Synchronizer<ApproxSyncPolicyWithCameraInfo> > async_with_info_;
     boost::shared_ptr<message_filters::Synchronizer<SyncPolicy> > sync_;
     boost::shared_ptr<message_filters::Synchronizer<ApproxSyncPolicy> > async_;
     boost::mutex mutex_;
@@ -83,23 +96,51 @@ namespace adding_images {
     double beta_;
     double gamma_;
 
+    void imageCallbackWithInfo(
+      const sensor_msgs::ImageConstPtr& msg1,
+      const sensor_msgs::ImageConstPtr& msg2,
+      const sensor_msgs::CameraInfoConstPtr& cam_info) {
+      do_work(msg1, msg2, cam_info->header.frame_id);
+    }
+
+    void imageCallback(const sensor_msgs::ImageConstPtr& msg1,
+                       const sensor_msgs::ImageConstPtr& msg2) {
+      do_work(msg1, msg2, msg1->header.frame_id);
+    }
+
     void subscribe() {
       NODELET_DEBUG("Subscribing to image topic.");
-      sub_image1_.subscribe(*it_, "image1", 1);
-      sub_image2_.subscribe(*it_, "image2", 1);
-      if (approximate_sync_) {
-        async_ =
-          boost::make_shared<message_filters::Synchronizer<ApproxSyncPolicy> >(
-            100);
-        async_->connectInput(sub_image1_, sub_image2_);
-        async_->registerCallback(
-          boost::bind(&AddingImagesNodelet::add, this, _1, _2));
+      sub_image1_.subscribe(*it_, "image1", 3);
+      sub_image2_.subscribe(*it_, "image2", 3);
+      sub_camera_info_.subscribe(*pnh_, "info", 3);
+      if (config_.use_camera_info) {
+        if (approximate_sync_) {
+          async_with_info_ = boost::make_shared<
+            message_filters::Synchronizer<ApproxSyncPolicyWithCameraInfo> >(100);
+          async_with_info_->connectInput(sub_image1_, sub_image2_, sub_camera_info_);
+          async_with_info_->registerCallback(boost::bind(
+            &AddingImagesNodelet::imageCallbackWithInfo, this, _1, _2, _3));
+        } else {
+          sync_with_info_ =
+            boost::make_shared<message_filters::Synchronizer<SyncPolicyWithCameraInfo> >(100);
+          sync_with_info_->connectInput(sub_image1_, sub_image2_, sub_camera_info_);
+          sync_with_info_->registerCallback(boost::bind(
+            &AddingImagesNodelet::imageCallbackWithInfo, this, _1, _2, _3));
+        }
       } else {
-        sync_ =
-          boost::make_shared<message_filters::Synchronizer<SyncPolicy> >(100);
-        sync_->connectInput(sub_image1_, sub_image2_);
-        sync_->registerCallback(
-          boost::bind(&AddingImagesNodelet::add, this, _1, _2));
+        if (approximate_sync_) {
+          async_ = boost::make_shared<
+            message_filters::Synchronizer<ApproxSyncPolicy> >(100);
+          async_->connectInput(sub_image1_, sub_image2_);
+          async_->registerCallback(
+            boost::bind(&AddingImagesNodelet::imageCallback, this, _1, _2));
+        } else {
+          sync_ =
+            boost::make_shared<message_filters::Synchronizer<SyncPolicy> >(100);
+          sync_->connectInput(sub_image1_, sub_image2_);
+          sync_->registerCallback(
+            boost::bind(&AddingImagesNodelet::imageCallback, this, _1, _2));
+        }
       }
     }
 
@@ -107,6 +148,7 @@ namespace adding_images {
       NODELET_DEBUG("Unsubscribing from image topic.");
       sub_image1_.unsubscribe();
       sub_image2_.unsubscribe();
+      sub_camera_info_.unsubscribe();
     }
 
     void reconfigureCallback(Config& config, uint32_t level) {
@@ -117,17 +159,35 @@ namespace adding_images {
       gamma_ = config.gamma;
     }
 
-    void add(const sensor_msgs::Image::ConstPtr& image_msg1,
-             const sensor_msgs::Image::ConstPtr& image_msg2) {
-      cv::Mat image1 =
-        cv_bridge::toCvShare(image_msg1, image_msg1->encoding)->image;
-      cv::Mat image2 =
-        cv_bridge::toCvShare(image_msg2, image_msg2->encoding)->image;
-      cv::Mat result_image;
-      cv::addWeighted(image1, alpha_, image2, beta_, gamma_, result_image);
-      img_pub_.publish(cv_bridge::CvImage(image_msg1->header,
-                                          image_msg1->encoding,
-                                          result_image).toImageMsg());
+    void do_work(const sensor_msgs::Image::ConstPtr& image_msg1,
+                 const sensor_msgs::Image::ConstPtr& image_msg2,
+                 const std::string input_frame_from_msg) {
+        boost::mutex::scoped_lock lock(mutex_);
+      // Work on the image.
+      try {
+        cv::Mat image1 =
+          cv_bridge::toCvShare(image_msg1, image_msg1->encoding)->image;
+        cv::Mat image2 =
+          cv_bridge::toCvShare(image_msg2, image_msg2->encoding)->image;
+
+        cv::Mat result_image;
+        cv::addWeighted(image1, alpha_, image2, beta_, gamma_, result_image);
+        //-- Show what you got
+        if (debug_view_) {
+          cv::namedWindow(window_name_, cv::WINDOW_AUTOSIZE);
+          cv::imshow(window_name_, result_image);
+          int c = cv::waitKey(1);
+        }
+        img_pub_.publish(cv_bridge::CvImage(image_msg1->header,
+                                            image_msg1->encoding,
+                                            result_image).toImageMsg());
+
+      } catch (cv::Exception& e) {
+        NODELET_ERROR("Image processing error: %s %s %s %i", e.err.c_str(),
+                      e.func.c_str(), e.file.c_str(), e.line);
+      }
+
+      prev_stamp_ = image_msg1->header.stamp;
     }
 
   public:
@@ -136,6 +196,13 @@ namespace adding_images {
       it_ = boost::shared_ptr<image_transport::ImageTransport>(
         new image_transport::ImageTransport(*nh_));
 
+      pnh_->param("debug_view", debug_view_, false);
+      if (debug_view_) {
+        always_subscribe_ = true;
+      }
+      prev_stamp_ = ros::Time(0, 0);
+
+      window_name_ = "AddingImages Demo";
       ////////////////////////////////////////////////////////
       // Dynamic Reconfigure
       ////////////////////////////////////////////////////////
