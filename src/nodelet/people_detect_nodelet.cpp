dc1478c6c814054eb4c19a2df98c27b9f43e61f6
Guilhem <gui2dev@users.noreply.github.com>
2018-06-03 08:55:58 +0200

    Change image encoding argument

--- src/nodelet/people_detect_nodelet.cpp
+++ src/nodelet/people_detect_nodelet.cpp
@@ -120,7 +120,7 @@ class PeopleDetectNodelet : public opencv_apps::Nodelet
     try
     {
       // Convert the image into something opencv can handle.
-      cv::Mat frame = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8)->image;
+      cv::Mat frame = cv_bridge::toCvShare(msg, msg->encoding)->image;
 
       // Messages
       opencv_apps::RectArrayStamped found_msg;
