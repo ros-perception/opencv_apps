70236428f722dab513eada4317b4e505e9ec8be7
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2019-04-20 07:54:31 +0000

    support catkin_lint and clang-format tests

--- .travis.sh
+++ .travis.sh
@@ -22,65 +22,131 @@ function travis_time_end {
     set -x
 }
 
+function setup {
+    travis_time_start setup.before_install
+    #before_install:
+    # Install ROS
+    sudo sh -c "echo \"deb http://packages.ros.org/ros-shadow-fixed/ubuntu `lsb_release -cs` main\" > /etc/apt/sources.list.d/ros-latest.list"
+    wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+    sudo apt-get update -qq
+    # Install ROS
+    sudo apt-get install -y -q python-catkin-pkg python-catkin-tools python-rosdep python-wstool python-rosinstall-generator ros-$ROS_DISTRO-catkin
+    source /opt/ros/$ROS_DISTRO/setup.bash
+    # Setup for rosdep
+    sudo rosdep init
+    rosdep update
+    travis_time_end
+
+    travis_time_start setup.install
+    #install:
+    mkdir -p ~/catkin_ws/src
+
+    # Add the package under test to the workspace.
+    cd ~/catkin_ws/src
+    ln -s $CI_SOURCE_PATH . # Link the repo we are testing to the new workspace
+
+    # Install all dependencies, using wstool and rosdep.
+    # wstool looks for a ROSINSTALL_FILE defined in before_install.
+    travis_time_end
+
+    travis_time_start setup.before_script
+    #before_script:
+    # source dependencies: install using wstool.
+    cd ~/catkin_ws/src
+    wstool init
+    #if [[ -f $ROSINSTALL_FILE ]] ; then wstool merge $ROSINSTALL_FILE ; fi
+    if [ "$OPENCV_VERSION" == 3 ]; then rosinstall_generator image_pipeline --upstream >> .rosinstall.opencv3; fi # need to recompile image_proc
+    if [ "$OPENCV_VERSION" == 3 ]; then rosinstall_generator compressed_image_transport --upstream >> .rosinstall.opencv3; fi # need to recompile compressed_image_transport
+    if [ "$OPENCV_VERSION" == 3 ]; then rosinstall_generator vision_opencv --upstream >> .rosinstall.opencv3; fi # need to recompile visoin_opencv
+    if [ "$OPENCV_VERSION" == 3 ]; then wstool merge .rosinstall.opencv3; fi # need to recompile visoin_opencv
+    wstool up
+    wstool info
+    if [ "$OPENCV_VERSION" == 3 ]; then  sed -i 's@libopencv-dev@opencv3@' */*/package.xml ; fi
+
+
+    # package depdencies: install using rosdep.
+    cd ~/catkin_ws
+    rosdep install -q -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
+    travis_time_end
+}
+
+function build {
+    travis_time_start build.script
+    source /opt/ros/$ROS_DISTRO/setup.bash
+    cd ~/catkin_ws
+    catkin build -p1 -j1 --no-status
+    travis_time_end
+}
+
+function run_test {
+    travis_time_start run_test.script
+    source /opt/ros/$ROS_DISTRO/setup.bash
+    cd ~/catkin_ws
+    catkin run_tests -p1 -j1 --no-status opencv_apps --no-deps
+    catkin_test_results --verbose build || catkin_test_results --all build
+    travis_time_end
+}
+
+function build_install {
+    travis_time_start build_install.script
+    source /opt/ros/$ROS_DISTRO/setup.bash
+    cd ~/catkin_ws
+    catkin clean -b --yes || catkin clean -b -a
+    catkin config --install
+    catkin build -p1 -j1 --no-status
+    travis_time_end
+}
+
+travis_time_start apt.before_install
 apt-get update -qq && apt-get install -y -q wget sudo lsb-release gnupg # for docker
 # set DEBIAN_FRONTEND=noninteractive
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
-
-travis_time_start setup.before_install
-#before_install:
-# Install ROS
-sudo sh -c "echo \"deb http://packages.ros.org/ros-shadow-fixed/ubuntu `lsb_release -cs` main\" > /etc/apt/sources.list.d/ros-latest.list"
-wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-sudo apt-get update -qq
-# Install ROS
-sudo apt-get install -y -q python-catkin-pkg python-catkin-tools python-rosdep python-wstool python-rosinstall-generator ros-$ROS_DISTRO-catkin
-source /opt/ros/$ROS_DISTRO/setup.bash
-# Setup for rosdep
-sudo rosdep init
-rosdep update
 travis_time_end
 
-travis_time_start setup.install
-#install:
-mkdir -p ~/catkin_ws/src
+if [ $TEST == "catkin_lint" ]; then
 
-# Add the package under test to the workspace.
-cd ~/catkin_ws/src
-ln -s $CI_SOURCE_PATH . # Link the repo we are testing to the new workspace
-
-# Install all dependencies, using wstool and rosdep.
-# wstool looks for a ROSINSTALL_FILE defined in before_install.
-travis_time_end
-
-travis_time_start setup.before_script
-#before_script:
-# source dependencies: install using wstool.
-cd ~/catkin_ws/src
-wstool init
-#if [[ -f $ROSINSTALL_FILE ]] ; then wstool merge $ROSINSTALL_FILE ; fi
-if [ $OPENCV_VERSION == 3 ]; then rosinstall_generator image_pipeline --upstream >> .rosinstall.opencv3; fi # need to recompile image_proc
-if [ $OPENCV_VERSION == 3 ]; then rosinstall_generator compressed_image_transport --upstream >> .rosinstall.opencv3; fi # need to recompile compressed_image_transport
-if [ $OPENCV_VERSION == 3 ]; then rosinstall_generator vision_opencv --upstream >> .rosinstall.opencv3; fi # need to recompile visoin_opencv
-if [ $OPENCV_VERSION == 3 ]; then wstool merge .rosinstall.opencv3; fi # need to recompile visoin_opencv
-wstool up
-wstool info
-if [ $OPENCV_VERSION == 3 ]; then  sed -i 's@libopencv-dev@opencv3@' */*/package.xml ; fi
+    travis_time_start catkin_lint.script
+    apt-get install -y -q python-pip
+    pip install catkin_lint rosdep
+    rosdep init
+    rosdep update
+    travis_time_end
+    ROS_DISTRO=melodic catkin_lint --resolve-env --strict $CI_SOURCE_PATH
 
 
-# package depdencies: install using rosdep.
-cd ~/catkin_ws
-rosdep install -q -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
-travis_time_end
+elif [ $TEST == "clang-format" ]; then
 
-# Compile and test.
-#script:
-travis_time_start setup.script
-source /opt/ros/$ROS_DISTRO/setup.bash
-cd ~/catkin_ws
-catkin build -p1 -j1 --no-status
-catkin run_tests -p1 -j1 --no-status opencv_apps --no-deps
-catkin_test_results --verbose build || catkin_test_results --all build
-catkin clean -b --yes || catkin clean -b -a
-catkin config --install
-catkin build -p1 -j1 --no-status
-travis_time_end
+    travis_time_start clang_format.script
+    apt-get install -y -q clang-format-3.9 git
+    find $CI_SOURCE_PATH -name '*.h' -or -name '*.hpp' -or -name '*.cpp' | xargs clang-format-3.9 -i -style=file
+    travis_time_end
+    git -C $CI_SOURCE_PATH --no-pager diff
+    git -C $CI_SOURCE_PATH diff-index --quiet HEAD -- .
+
+elif [ $TEST == "clang-tidy" ]; then
+
+    setup
+
+    travis_time_start clang_tidy.script
+    apt-get install -y -q  clang-tidy clang-tools
+    cd ~/catkin_ws
+    catkin config --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+    travis_time_end
+
+    build
+
+    travis_time_start clang_tidy.script
+    for file in $(find ~/catkin_ws/build -name compile_commands.json) ; do
+	    run-clang-tidy -fix -p $(dirname $file)
+    done
+    travis_time_end
+    git -C $CI_SOURCE_PATH --no-pager diff
+    git -C $CI_SOURCE_PATH diff-index --quiet HEAD -- .
+
+else
+    # Compile and test.
+    setup
+    build
+    run_test
+    build_install
+fi
