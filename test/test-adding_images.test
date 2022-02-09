ros-perception/opencv_apps

commit 4c4caef9a812534cfd2426b6fed30b0a33f4d3d3

fix code by run-clang-tidy -fix

include/opencv_apps/nodelet.h
@@ -40,6 +40,11 @@
 #include <boost/thread.hpp>
 #include <image_transport/image_transport.h>
 
+// https://stackoverflow.com/questions/10496824/how-to-define-nullptr-for-supporting-both-c03-and-c11
+#if !defined(nullptr)
+#define nullptr NULL
+#endif
+
 namespace opencv_apps
 {
 /** @brief

src/nodelet/adding_images_nodelet.cpp
@@ -101,15 +101,15 @@ private:
   void imageCallbackWithInfo(const sensor_msgs::ImageConstPtr& msg1, const sensor_msgs::ImageConstPtr& msg2,
                              const sensor_msgs::CameraInfoConstPtr& cam_info)
   {
-    do_work(msg1, msg2, cam_info->header.frame_id);
+    doWork(msg1, msg2, cam_info->header.frame_id);
   }
 
   void imageCallback(const sensor_msgs::ImageConstPtr& msg1, const sensor_msgs::ImageConstPtr& msg2)
   {
-    do_work(msg1, msg2, msg1->header.frame_id);
+    doWork(msg1, msg2, msg1->header.frame_id);
   }
 
-  void subscribe()
+  void subscribe() override
   {
     NODELET_DEBUG("Subscribing to image topic.");
     sub_image1_.subscribe(*it_, "image1", 3);
@@ -148,7 +148,7 @@ private:
     }
   }
 
-  void unsubscribe()
+  void unsubscribe() override
   {
     NODELET_DEBUG("Unsubscribing from image topic.");
     sub_image1_.unsubscribe();
@@ -173,8 +173,8 @@ private:
     gamma_ = config.gamma;
   }
 
-  void do_work(const sensor_msgs::Image::ConstPtr& image_msg1, const sensor_msgs::Image::ConstPtr& image_msg2,
-               const std::string input_frame_from_msg)
+  void doWork(const sensor_msgs::Image::ConstPtr& image_msg1, const sensor_msgs::Image::ConstPtr& image_msg2,
+              const std::string& input_frame_from_msg)
   {
     boost::mutex::scoped_lock lock(mutex_);
     // Work on the image.
@@ -195,14 +195,14 @@ private:
         int new_rows = std::max(image1.rows, image2.rows);
         int new_cols = std::max(image1.cols, image2.cols);
         // if ( new_rows != image1.rows || new_cols != image1.cols ) {
-        cv::Mat image1_ = cv::Mat(new_rows, new_cols, image1.type());
-        image1.copyTo(image1_(cv::Rect(0, 0, image1.cols, image1.rows)));
-        image1 = image1_.clone();  // need clone becuase toCvShare??
+        cv::Mat image1 = cv::Mat(new_rows, new_cols, image1.type());
+        image1.copyTo(image1(cv::Rect(0, 0, image1.cols, image1.rows)));
+        image1 = image1.clone();  // need clone becuase toCvShare??
 
         // if ( new_rows != image2.rows || new_cols != image2.cols ) {
-        cv::Mat image2_ = cv::Mat(new_rows, new_cols, image2.type());
-        image2.copyTo(image2_(cv::Rect(0, 0, image2.cols, image2.rows)));
-        image2 = image2_.clone();
+        cv::Mat image2 = cv::Mat(new_rows, new_cols, image2.type());
+        image2.copyTo(image2(cv::Rect(0, 0, image2.cols, image2.rows)));
+        image2 = image2.clone();
       }
       cv::addWeighted(image1, alpha_, image2, beta_, gamma_, result_image);
       //-- Show what you got
@@ -241,7 +241,7 @@ private:
   }
 
 public:
-  virtual void onInit()
+  void onInit() override
   {
     Nodelet::onInit();
     it_ = boost::shared_ptr<image_transport::ImageTransport>(new image_transport::ImageTransport(*nh_));
@@ -275,7 +275,7 @@ namespace adding_images
 class AddingImagesNodelet : public opencv_apps::AddingImagesNodelet
 {
 public:
-  virtual void onInit()
+  void onInit() override
   {
     ROS_WARN("DeprecationWarning: Nodelet adding_images/adding_images is deprecated, "
              "and renamed to opencv_apps/adding_images.");

src/nodelet/camshift_nodelet.cpp
@@ -99,7 +99,7 @@ class CamShiftNodelet : public opencv_apps::Nodelet
   cv::Mat hist, histimg;
   // cv::Mat hsv;
 
-  static void onMouse(int event, int x, int y, int, void*)
+  static void onMouse(int event, int x, int y, int /*unused*/, void* /*unused*/)
   {
     on_mouse_update_ = true;
     on_mouse_event_ = event;
@@ -124,20 +124,20 @@ class CamShiftNodelet : public opencv_apps::Nodelet
 
   void imageCallbackWithInfo(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info)
   {
-    do_work(msg, cam_info->header.frame_id);
+    doWork(msg, cam_info->header.frame_id);
   }
 
   void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   {
-    do_work(msg, msg->header.frame_id);
+    doWork(msg, msg->header.frame_id);
   }
 
-  static void trackbarCallback(int, void*)
+  static void trackbarCallback(int /*unused*/, void* /*unused*/)
   {
     need_config_update_ = true;
   }
 
-  void do_work(const sensor_msgs::ImageConstPtr& msg, const std::string input_frame_from_msg)
+  void doWork(const sensor_msgs::ImageConstPtr& msg, const std::string& input_frame_from_msg)
   {
     // Work on the image.
     try
@@ -158,7 +158,7 @@ class CamShiftNodelet : public opencv_apps::Nodelet
 
         cv::namedWindow(window_name_, cv::WINDOW_AUTOSIZE);
 
-        cv::setMouseCallback(window_name_, onMouse, 0);
+        cv::setMouseCallback(window_name_, onMouse, nullptr);
         cv::createTrackbar("Vmin", window_name_, &vmin_, 256, trackbarCallback);
         cv::createTrackbar("Vmax", window_name_, &vmax_, 256, trackbarCallback);
         cv::createTrackbar("Smin", window_name_, &smin_, 256, trackbarCallback);
@@ -212,9 +212,9 @@ class CamShiftNodelet : public opencv_apps::Nodelet
 
         if (trackObject)
         {
-          int _vmin = vmin_, _vmax = vmax_;
+          int vmin = vmin_, vmax = vmax_;
 
-          cv::inRange(hsv, cv::Scalar(0, smin_, MIN(_vmin, _vmax)), cv::Scalar(180, 256, MAX(_vmin, _vmax)), mask);
+          cv::inRange(hsv, cv::Scalar(0, smin_, MIN(vmin, vmax)), cv::Scalar(180, 256, MAX(vmin, vmax)), mask);
           int ch[] = { 0, 0 };
           hue.create(hsv.size(), hsv.depth());
           cv::mixChannels(&hsv, 1, &hue, 1, ch, 1);
@@ -222,7 +222,7 @@ class CamShiftNodelet : public opencv_apps::Nodelet
           if (trackObject < 0)
           {
             cv::Mat roi(hue, selection), maskroi(mask, selection);
-            cv::calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
+            cv::calcHist(&roi, 1, nullptr, maskroi, hist, 1, &hsize, &phranges);
             cv::normalize(hist, hist, 0, 255, cv::NORM_MINMAX);
             std::vector<float> hist_value;
             hist_value.resize(hsize);
@@ -236,7 +236,7 @@ class CamShiftNodelet : public opencv_apps::Nodelet
             trackObject = 1;
 
             histimg = cv::Scalar::all(0);
-            int binW = histimg.cols / hsize;
+            int bin_w = histimg.cols / hsize;
             cv::Mat buf(1, hsize, CV_8UC3);
             for (int i = 0; i < hsize; i++)
               buf.at<cv::Vec3b>(i) = cv::Vec3b(cv::saturate_cast<uchar>(i * 180. / hsize), 255, 255);
@@ -245,14 +245,14 @@ class CamShiftNodelet : public opencv_apps::Nodelet
             for (int i = 0; i < hsize; i++)
             {
               int val = cv::saturate_cast<int>(hist.at<float>(i) * histimg.rows / 255);
-              cv::rectangle(histimg, cv::Point(i * binW, histimg.rows), cv::Point((i + 1) * binW, histimg.rows - val),
+              cv::rectangle(histimg, cv::Point(i * bin_w, histimg.rows), cv::Point((i + 1) * bin_w, histimg.rows - val),
                             cv::Scalar(buf.at<cv::Vec3b>(i)), -1, 8);
             }
           }
 
-          cv::calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
+          cv::calcBackProject(&hue, 1, nullptr, hist, backproj, &phranges);
           backproj &= mask;
-          cv::RotatedRect trackBox = cv::CamShift(
+          cv::RotatedRect track_box = cv::CamShift(
               backproj, trackWindow, cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 10, 1));
           if (trackWindow.area() <= 1)
           {
@@ -265,18 +265,18 @@ class CamShiftNodelet : public opencv_apps::Nodelet
           if (backprojMode)
             cv::cvtColor(backproj, frame, cv::COLOR_GRAY2BGR);
 #ifndef CV_VERSION_EPOCH
-          cv::ellipse(frame, trackBox, cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
+          cv::ellipse(frame, track_box, cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
 #else
-          cv::ellipse(frame, trackBox, cv::Scalar(0, 0, 255), 3, CV_AA);
+          cv::ellipse(frame, track_box, cv::Scalar(0, 0, 255), 3, CV_AA);
 #endif
 
-          rect_msg.rect.angle = trackBox.angle;
+          rect_msg.rect.angle = track_box.angle;
           opencv_apps::Point2D point_msg;
           opencv_apps::Size size_msg;
-          point_msg.x = trackBox.center.x;
-          point_msg.y = trackBox.center.y;
-          size_msg.width = trackBox.size.width;
-          size_msg.height = trackBox.size.height;
+          point_msg.x = track_box.center.x;
+          point_msg.y = track_box.center.y;
+          size_msg.width = track_box.size.width;
+          size_msg.height = track_box.size.height;
           rect_msg.rect.center = point_msg;
           rect_msg.rect.size = size_msg;
         }
@@ -338,7 +338,7 @@ class CamShiftNodelet : public opencv_apps::Nodelet
     prev_stamp_ = msg->header.stamp;
   }
 
-  void subscribe()
+  void subscribe() override
   {
     NODELET_DEBUG("Subscribing to image topic.");
     if (config_.use_camera_info)
@@ -347,7 +347,7 @@ class CamShiftNodelet : public opencv_apps::Nodelet
       img_sub_ = it_->subscribe("image", queue_size_, &CamShiftNodelet::imageCallback, this);
   }
 
-  void unsubscribe()
+  void unsubscribe() override
   {
     NODELET_DEBUG("Unsubscribing from image topic.");
     img_sub_.shutdown();
@@ -355,7 +355,7 @@ class CamShiftNodelet : public opencv_apps::Nodelet
   }
 
 public:
-  virtual void onInit()
+  void onInit() override
   {
     Nodelet::onInit();
     it_ = boost::shared_ptr<image_transport::ImageTransport>(new image_transport::ImageTransport(*nh_));
@@ -416,7 +416,7 @@ public:
       trackWindow = cv::Rect(0, 0, 640, 480);  //
 
       histimg = cv::Scalar::all(0);
-      int binW = histimg.cols / hsize;
+      int bin_w = histimg.cols / hsize;
       cv::Mat buf(1, hsize, CV_8UC3);
       for (int i = 0; i < hsize; i++)
         buf.at<cv::Vec3b>(i) = cv::Vec3b(cv::saturate_cast<uchar>(i * 180. / hsize), 255, 255);
@@ -425,7 +425,7 @@ public:
       for (int i = 0; i < hsize; i++)
       {
         int val = cv::saturate_cast<int>(hist.at<float>(i) * histimg.rows / 255);
-        cv::rectangle(histimg, cv::Point(i * binW, histimg.rows), cv::Point((i + 1) * binW, histimg.rows - val),
+        cv::rectangle(histimg, cv::Point(i * bin_w, histimg.rows), cv::Point((i + 1) * bin_w, histimg.rows - val),
                       cv::Scalar(buf.at<cv::Vec3b>(i)), -1, 8);
       }
     }
@@ -444,7 +444,7 @@ namespace camshift
 class CamShiftNodelet : public opencv_apps::CamShiftNodelet
 {
 public:
-  virtual void onInit()
+  void onInit() override
   {
     ROS_WARN("DeprecationWarning: Nodelet camshift/camshift is deprecated, "
              "and renamed to opencv_apps/camshift.");

src/nodelet/color_filter_nodelet.cpp
@@ -99,15 +99,15 @@ protected:
 
   void imageCallbackWithInfo(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info)
   {
-    do_work(msg, cam_info->header.frame_id);
+    doWork(msg, cam_info->header.frame_id);
   }
 
   void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   {
-    do_work(msg, msg->header.frame_id);
+    doWork(msg, msg->header.frame_id);
   }
 
-  void do_work(const sensor_msgs::ImageConstPtr& image_msg, const std::string input_frame_from_msg)
+  void doWork(const sensor_msgs::ImageConstPtr& image_msg, const std::string& input_frame_from_msg)
   {
     // Work on the image.
     try
@@ -148,7 +148,7 @@ protected:
       NODELET_ERROR("Image processing error: %s %s %s %i", e.err.c_str(), e.func.c_str(), e.file.c_str(), e.line);
     }
   }
-  void subscribe()
+  void subscribe() override
   {
     NODELET_DEBUG("Subscribing to image topic.");
     if (config_.use_camera_info)
@@ -157,7 +157,7 @@ protected:
       img_sub_ = it_->subscribe("image", queue_size_, &ColorFilterNodelet::imageCallback, this);
   }
 
-  void unsubscribe()
+  void unsubscribe() override
   {
     NODELET_DEBUG("Unsubscribing from image topic.");
     img_sub_.shutdown();
@@ -165,7 +165,7 @@ protected:
   }
 
 public:
-  virtual void onInit()
+  void onInit() override
   {
     Nodelet::onInit();
     it_ = boost::shared_ptr<image_transport::ImageTransport>(new image_transport::ImageTransport(*nh_));
@@ -201,7 +201,7 @@ protected:
   int g_min_;
   int g_max_;
 
-  virtual void reconfigureCallback(opencv_apps::RGBColorFilterConfig& config, uint32_t level)
+  void reconfigureCallback(opencv_apps::RGBColorFilterConfig& config, uint32_t level) override
   {
     boost::mutex::scoped_lock lock(mutex_);
     config_ = config;
@@ -226,13 +226,13 @@ protected:
     upper_color_range_ = cv::Scalar(b_max_, g_max_, r_max_);
   }
 
-  virtual void filter(const cv::Mat& input_image, cv::Mat& output_image)
+  void filter(const cv::Mat& input_image, cv::Mat& output_image) override
   {
     cv::inRange(input_image, lower_color_range_, upper_color_range_, output_image);
   }
 
 protected:
-  virtual void onInit()
+  void onInit() override
   {
     r_max_ = 255;
     r_min_ = 0;
@@ -258,7 +258,7 @@ protected:
   int l_min_;
   int l_max_;
 
-  virtual void reconfigureCallback(opencv_apps::HLSColorFilterConfig& config, uint32_t level)
+  void reconfigureCallback(opencv_apps::HLSColorFilterConfig& config, uint32_t level) override
   {
     boost::mutex::scoped_lock lock(mutex_);
     config_ = config;
@@ -281,7 +281,7 @@ protected:
     upper_color_range_ = cv::Scalar(h_max_ / 2, l_max_, s_max_, 0);
   }
 
-  virtual void filter(const cv::Mat& input_image, cv::Mat& output_image)
+  void filter(const cv::Mat& input_image, cv::Mat& output_image) override
   {
     cv::Mat hls_image;
     cv::cvtColor(input_image, hls_image, cv::COLOR_BGR2HLS);
@@ -303,7 +303,7 @@ protected:
   }
 
 public:
-  virtual void onInit()
+  void onInit() override
   {
     h_max_ = 360;
     h_min_ = 0;
@@ -326,7 +326,7 @@ protected:
   int v_min_;
   int v_max_;
 
-  virtual void reconfigureCallback(opencv_apps::HSVColorFilterConfig& config, uint32_t level)
+  void reconfigureCallback(opencv_apps::HSVColorFilterConfig& config, uint32_t level) override
   {
     boost::mutex::scoped_lock lock(mutex_);
     config_ = config;
@@ -349,7 +349,7 @@ protected:
     upper_color_range_ = cv::Scalar(h_max_ / 2, s_max_, v_max_, 0);
   }
 
-  virtual void filter(const cv::Mat& input_image, cv::Mat& output_image)
+  void filter(const cv::Mat& input_image, cv::Mat& output_image) override
   {
     cv::Mat hsv_image;
     cv::cvtColor(input_image, hsv_image, cv::COLOR_BGR2HSV);
@@ -371,7 +371,7 @@ protected:
   }
 
 public:
-  virtual void onInit()
+  void onInit() override
   {
     h_max_ = 360;
     h_min_ = 0;
@@ -391,7 +391,7 @@ namespace color_filter
 class RGBColorFilterNodelet : public opencv_apps::RGBColorFilterNodelet
 {
 public:
-  virtual void onInit()
+  void onInit() override
   {
     ROS_WARN("DeprecationWarning: Nodelet rgb_color_filter/rgb_color_filter is deprecated, "
              "and renamed to opencv_apps/rgb_color_filter.");
@@ -401,7 +401,7 @@ public:
 class HLSColorFilterNodelet : public opencv_apps::HLSColorFilterNodelet
 {
 public:
-  virtual void onInit()
+  void onInit() override
   {
     ROS_WARN("DeprecationWarning: Nodelet hls_color_filter/hls_color_filter is deprecated, "
              "and renamed to opencv_apps/hls_color_filter.");
@@ -411,7 +411,7 @@ public:
 class HSVColorFilterNodelet : public opencv_apps::HSVColorFilterNodelet
 {
 public:
-  virtual void onInit()
+  void onInit() override
   {
     ROS_WARN("DeprecationWarning: Nodelet hsv_color_filter/hsv_color_filter is deprecated, "
              "and renamed to opencv_apps/hsv_color_filter.");

src/nodelet/contour_moments_nodelet.cpp
@@ -58,7 +58,7 @@ namespace opencv_apps
 {
 // https://stackoverflow.com/questions/13495207/opencv-c-sorting-contours-by-their-contourarea
 // comparison function object
-bool compareContourAreas(std::vector<cv::Point> contour1, std::vector<cv::Point> contour2)
+bool compareContourAreas(const std::vector<cv::Point>& contour1, const std::vector<cv::Point>& contour2)
 {
   double i = fabs(contourArea(cv::Mat(contour1)));
   double j = fabs(contourArea(cv::Mat(contour2)));
@@ -103,20 +103,20 @@ class ContourMomentsNodelet : public opencv_apps::Nodelet
 
   void imageCallbackWithInfo(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info)
   {
-    do_work(msg, cam_info->header.frame_id);
+    doWork(msg, cam_info->header.frame_id);
   }
 
   void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   {
-    do_work(msg, msg->header.frame_id);
+    doWork(msg, msg->header.frame_id);
   }
 
-  static void trackbarCallback(int, void*)
+  static void trackbarCallback(int /*unused*/, void* /*unused*/)
   {
     need_config_update_ = true;
   }
 
-  void do_work(const sensor_msgs::ImageConstPtr& msg, const std::string input_frame_from_msg)
+  void doWork(const sensor_msgs::ImageConstPtr& msg, const std::string& input_frame_from_msg)
   {
     // Work on the image.
     try
@@ -249,7 +249,7 @@ class ContourMomentsNodelet : public opencv_apps::Nodelet
     prev_stamp_ = msg->header.stamp;
   }
 
-  void subscribe()
+  void subscribe() override
   {
     NODELET_DEBUG("Subscribing to image topic.");
     if (config_.use_camera_info)
@@ -258,7 +258,7 @@ class ContourMomentsNodelet : public opencv_apps::Nodelet
       img_sub_ = it_->subscribe("image", queue_size_, &ContourMomentsNodelet::imageCallback, this);
   }
 
-  void unsubscribe()
+  void unsubscribe() override
   {
     NODELET_DEBUG("Unsubscribing from image topic.");
     img_sub_.shutdown();
@@ -266,7 +266,7 @@ class ContourMomentsNodelet : public opencv_apps::Nodelet
   }
 
 public:
-  virtual void onInit()
+  void onInit() override
   {
     Nodelet::onInit();
     it_ = boost::shared_ptr<image_transport::ImageTransport>(new image_transport::ImageTransport(*nh_));
@@ -300,7 +300,7 @@ namespace contour_moments
 class ContourMomentsNodelet : public opencv_apps::ContourMomentsNodelet
 {
 public:
-  virtual void onInit()
+  void onInit() override
   {
     ROS_WARN("DeprecationWarning: Nodelet contour_moments/contour_moments is deprecated, "
              "and renamed to opencv_apps/contour_moments.");

src/nodelet/convex_hull_nodelet.cpp
@@ -95,20 +95,20 @@ class ConvexHullNodelet : public opencv_apps::Nodelet
 
   void imageCallbackWithInfo(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info)
   {
-    do_work(msg, cam_info->header.frame_id);
+    doWork(msg, cam_info->header.frame_id);
   }
 
   void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   {
-    do_work(msg, msg->header.frame_id);
+    doWork(msg, msg->header.frame_id);
   }
 
-  static void trackbarCallback(int, void*)
+  static void trackbarCallback(int /*unused*/, void* /*unused*/)
   {
     need_config_update_ = true;
   }
 
-  void do_work(const sensor_msgs::ImageConstPtr& msg, const std::string input_frame_from_msg)
+  void doWork(const sensor_msgs::ImageConstPtr& msg, const std::string& input_frame_from_msg)
   {
     // Work on the image.
     try
@@ -168,11 +168,11 @@ class ConvexHullNodelet : public opencv_apps::Nodelet
         cv::drawContours(drawing, hull, (int)i, color, 4, 8, std::vector<cv::Vec4i>(), 0, cv::Point());
 
         opencv_apps::Contour contour_msg;
-        for (size_t j = 0; j < hull[i].size(); j++)
+        for (const cv::Point& j : hull[i])
         {
           opencv_apps::Point2D point_msg;
-          point_msg.x = hull[i][j].x;
-          point_msg.y = hull[i][j].y;
+          point_msg.x = j.x;
+          point_msg.y = j.y;
           contour_msg.points.push_back(point_msg);
         }
         contours_msg.contours.push_back(contour_msg);
@@ -207,7 +207,7 @@ class ConvexHullNodelet : public opencv_apps::Nodelet
     prev_stamp_ = msg->header.stamp;
   }
 
-  void subscribe()
+  void subscribe() override
   {
     NODELET_DEBUG("Subscribing to image topic.");
     if (config_.use_camera_info)
@@ -216,7 +216,7 @@ class ConvexHullNodelet : public opencv_apps::Nodelet
       img_sub_ = it_->subscribe("image", queue_size_, &ConvexHullNodelet::imageCallback, this);
   }
 
-  void unsubscribe()
+  void unsubscribe() override
   {
     NODELET_DEBUG("Unsubscribing from image topic.");
     img_sub_.shutdown();
@@ -224,7 +224,7 @@ class ConvexHullNodelet : public opencv_apps::Nodelet
   }
 
 public:
-  virtual void onInit()
+  void onInit() override
   {
     Nodelet::onInit();
     it_ = boost::shared_ptr<image_transport::ImageTransport>(new image_transport::ImageTransport(*nh_));
@@ -258,7 +258,7 @@ namespace convex_hull
 class ConvexHullNodelet : public opencv_apps::ConvexHullNodelet
 {
 public:
-  virtual void onInit()
+  void onInit() override
   {
     ROS_WARN("DeprecationWarning: Nodelet convex_hull/convex_hull is deprecated, "
              "and renamed to opencv_apps/convex_hull.");

src/nodelet/corner_harris_nodelet.cpp
@@ -90,20 +90,20 @@ class CornerHarrisNodelet : public opencv_apps::Nodelet
 
   void imageCallbackWithInfo(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info)
   {
-    do_work(msg, cam_info->header.frame_id);
+    doWork(msg, cam_info->header.frame_id);
   }
 
   void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   {
-    do_work(msg, msg->header.frame_id);
+    doWork(msg, msg->header.frame_id);
   }
 
-  static void trackbarCallback(int, void*)
+  static void trackbarCallback(int /*unused*/, void* /*unused*/)
   {
     need_config_update_ = true;
   }
 
-  void do_work(const sensor_msgs::ImageConstPtr& image_msg, const std::string input_frame_from_msg)
+  void doWork(const sensor_msgs::ImageConstPtr& image_msg, const std::string& input_frame_from_msg)
   {
     // Work on the image.
     try
@@ -128,12 +128,12 @@ class CornerHarrisNodelet : public opencv_apps::Nodelet
       }
 
       /// Detector parameters
-      int blockSize = 2;
-      int apertureSize = 3;
+      int block_size = 2;
+      int aperture_size = 3;
       double k = 0.04;
 
       /// Detecting corners
-      cv::cornerHarris(src_gray, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
+      cv::cornerHarris(src_gray, dst, block_size, aperture_size, k, cv::BORDER_DEFAULT);
 
       /// Normalizing
       cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
@@ -182,7 +182,7 @@ class CornerHarrisNodelet : public opencv_apps::Nodelet
     }
   }
 
-  void subscribe()
+  void subscribe() override
   {
     NODELET_DEBUG("Subscribing to image topic.");
     if (config_.use_camera_info)
@@ -191,7 +191,7 @@ class CornerHarrisNodelet : public opencv_apps::Nodelet
       img_sub_ = it_->subscribe("image", queue_size_, &CornerHarrisNodelet::imageCallback, this);
   }
 
-  void unsubscribe()
+  void unsubscribe() override
   {
     NODELET_DEBUG("Unsubscribing from image topic.");
     img_sub_.shutdown();
@@ -199,7 +199,7 @@ class CornerHarrisNodelet : public opencv_apps::Nodelet
   }
 
 public:
-  virtual void onInit()
+  void onInit() override
   {
     Nodelet::onInit();
     it_ = boost::shared_ptr<image_transport::ImageTransport>(new image_transport::ImageTransport(*nh_));
@@ -232,7 +232,7 @@ namespace corner_harris
 class CornerHarrisNodelet : public opencv_apps::CornerHarrisNodelet
 {
 public:
-  virtual void onInit()
+  void onInit() override
   {
     ROS_WARN("DeprecationWarning: Nodelet corner_harris/corner_harris is deprecated, "
              "and renamed to opencv_apps/corner_harris.");

src/nodelet/discrete_fourier_transform_nodelet.cpp
@@ -75,15 +75,15 @@ class DiscreteFourierTransformNodelet : public opencv_apps::Nodelet
 
   void imageCallbackWithInfo(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info)
   {
-    do_work(msg, cam_info->header.frame_id);
+    doWork(msg, cam_info->header.frame_id);
   }
 
   void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   {
-    do_work(msg, msg->header.frame_id);
+    doWork(msg, msg->header.frame_id);
   }
 
-  void subscribe()
+  void subscribe() override
   {
     NODELET_DEBUG("Subscribing to image topic.");
     if (config_.use_camera_info)
@@ -93,7 +93,7 @@ class DiscreteFourierTransformNodelet : public opencv_apps::Nodelet
       img_sub_ = it_->subscribe("image", queue_size_, &DiscreteFourierTransformNodelet::imageCallback, this);
   }
 
-  void unsubscribe()
+  void unsubscribe() override
   {
     NODELET_DEBUG("Unsubscribing from image topic.");
     img_sub_.shutdown();
@@ -106,7 +106,7 @@ class DiscreteFourierTransformNodelet : public opencv_apps::Nodelet
     config_ = config;
   }
 
-  void do_work(const sensor_msgs::Image::ConstPtr& image_msg, const std::string input_frame_from_msg)
+  void doWork(const sensor_msgs::Image::ConstPtr& image_msg, const std::string& input_frame_from_msg)
   {
     // Work on the image.
     try
@@ -185,7 +185,7 @@ class DiscreteFourierTransformNodelet : public opencv_apps::Nodelet
   }
 
 public:
-  virtual void onInit()
+  void onInit() override
   {
     Nodelet::onInit();
     it_ = boost::shared_ptr<image_transport::ImageTransport>(new image_transport::ImageTransport(*nh_));
@@ -216,7 +216,7 @@ namespace discrete_fourier_transform
 class DiscreteFourierTransformNodelet : public opencv_apps::DiscreteFourierTransformNodelet
 {
 public:
-  virtual void onInit()
+  void onInit() override
   {
     ROS_WARN("DeprecationWarning: Nodelet discrete_fourier_transform/discrete_fourier_transform is deprecated, "
              "and renamed to opencv_apps/discrete_fourier_transform.");

commit a5ac13896d41398a717a80406ce0fd56d2ad31f7

add test to check when adding two images, whcih has different size.
This test should fix the bug introduced in https://github.com/ros-perception/opencv_apps/commit/4c4caef9a812534cfd2426b6fed30b0a33f4d3d3

test/test-adding_images.test
@@ -23,12 +23,36 @@
       <arg name="approximate_sync" value="false" />
     </include>
 
+    <!-- Check bug introduced in 4c4caef. when image1.rows != image2.rows || image1.cols != image2.cols)
+         see https://github.com/ros-perception/opencv_apps/issues/99 -->
+    <node pkg="nodelet" type="nodelet" name="resize"
+          args="standalone image_proc/resize" output="screen">
+      <remap from="image" to="image_rect_color" />
+      <param name="scale_height" value="0.5" />
+      <param name="scale_width" value="0.5" />
+    </node>
+    <include file="$(find opencv_apps)/launch/adding_images.launch" >
+      <arg name="debug_view" value="$(arg gui)" />
+      <arg name="image1" value="image_rect_color" />
+      <arg name="image2" value="resize/image" />
+      <arg name="alpha" value="0.5" />
+      <arg name="gamma" value="0" />
+      <arg name="approximate_sync" value="false" />
+      <arg name="node_name" default="adding_images_resize" />
+    </include>
+
     <!-- Test Codes -->
     <node name="adding_images_saver_result" pkg="image_view" type="image_saver" args="image:=adding_images/image" >
       <param name="filename_format" value="$(find opencv_apps)/test/adding_images_result.png"/>
     </node>
     <param name="adding_images_test/topic" value="adding_images/image" />
     <test test-name="adding_images_test" pkg="rostest" type="hztest" name="adding_images_test" time-limit="120" >
+      <param name="hz" value="30" />
+      <param name="hzerror" value="10.0" />
+      <param name="test_duration" value="1.0" />
+    </test>
+    <param name="adding_images_test_resize/topic" value="adding_images_resize/image" />
+    <test test-name="adding_images_test_resize" pkg="rostest" type="hztest" name="adding_images_test_resize" time-limit="120" >
       <param name="hz" value="30" />
       <param name="hzerror" value="10.0" />
       <param name="test_duration" value="1.0" />