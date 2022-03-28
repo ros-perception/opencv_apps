b95a7ba7c6c95c4f560e170dc10025f141792428
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2022-03-28 15:16:12 +0900

    use distutils.core for hydro,

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from setuptools import setup
+import catkin_pkg
+from distutils.version import LooseVersion
+if LooseVersion(catkin_pkg.__version__) <= LooseVersion('0.4.8'):
+    # on hydro we need to use distutils
+    from distutils.core import setup
+else:
+    from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
