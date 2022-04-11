2414af69fe1aac36652fe3b60b2f887d73672847
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2022-04-11 13:29:24 +0900

    [objectness] fix coding style based on clang-format

--- src/nodelet/objectness_nodelet.cpp
+++ src/nodelet/objectness_nodelet.cpp
@@ -46,146 +46,167 @@
 #include "opencv_apps/ObjectnessConfig.h"
 #include "opencv_apps/RectArrayStamped.h"
 
-namespace opencv_apps{
-    class ObjectnessNodelet : public opencv_apps::Nodelet
+namespace opencv_apps
+{
+class ObjectnessNodelet : public opencv_apps::Nodelet
+{
+  std::string window_name_;
+
+  image_transport::Subscriber img_sub_;
+  image_transport::CameraSubscriber cam_sub_;
+  ros::Publisher msg_pub_;
+
+  std::shared_ptr<image_transport::ImageTransport> it_;
+
+  typedef opencv_apps::ObjectnessConfig Config;
+  typedef dynamic_reconfigure::Server<Config> ReconfigureServer;
+
+  Config config_;
+  std::shared_ptr<ReconfigureServer> reconfigure_server_;
+
+  int queue_size_;
+  bool debug_view_;
+
+  boost::mutex mutex_;
+
+  cv::Ptr<cv::saliency::Saliency> objectnessAlgorithm;
+
+  int nss_;
+  std::string training_path_;
+  int max_objectness_;
+
+  void reconfigureCallback(Config& new_config, uint32_t level)
+  {
+    config_ = new_config;
+    nss_ = config_.nss;
+    max_objectness_ = config_.max_objectness;
+  }
+
+  void imageCallbackWithInfo(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info)
+  {
+    doWork(msg, cam_info->header.frame_id);
+  }
+
+  void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+  {
+    doWork(msg, msg->header.frame_id);
+  }
+
+  void doWork(const sensor_msgs::ImageConstPtr& msg, const std::string& input_frame_from_msg)
+  {
+    try
     {
-        std::string window_name_;
+      // declaration
+      std::vector<cv::Vec4i> objectnessBoxes_;
+      cv::Mat frame_, debug_frame_;
+      opencv_apps::RectArrayStamped rects_;
 
-        image_transport::Subscriber img_sub_;
-        image_transport::CameraSubscriber cam_sub_;
-        ros::Publisher msg_pub_;
+      // convert the image msg to cv object
+      if (msg->encoding == sensor_msgs::image_encodings::BGR8)
+      {
+        frame_ = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8)->image;
+      }
+      else if (msg->encoding == sensor_msgs::image_encodings::RGB8)
+      {
+        frame_ = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::RGB8)->image;
+        cv::cvtColor(frame_, frame_, CV_RGB2BGR);
+      }
+      else if (msg->encoding == sensor_msgs::image_encodings::MONO8)
+      {
+        frame_ = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::MONO8)->image;
+      }
+      else
+      {
+        NODELET_ERROR_STREAM("Not supported image encoding: " << msg->encoding);
+      }
 
-        std::shared_ptr<image_transport::ImageTransport> it_;
+      if (debug_view_)
+      {
+        cv::namedWindow(window_name_, cv::WINDOW_AUTOSIZE);
+        debug_frame_ = frame_.clone();
+      }
 
-        typedef opencv_apps::ObjectnessConfig Config;
-        typedef dynamic_reconfigure::Server<Config> ReconfigureServer;
+      // reconfigure
+      objectnessAlgorithm.dynamicCast<cv::saliency::ObjectnessBING>()->setNSS(nss_);
 
-        Config config_;
-        std::shared_ptr<ReconfigureServer> reconfigure_server_;
-
-        int queue_size_;
-        bool debug_view_;
-
-        boost::mutex mutex_;
-
-        cv::Ptr<cv::saliency::Saliency> objectnessAlgorithm;
-
-        int nss_;
-        std::string training_path_;
-        int max_objectness_;
-
-        void reconfigureCallback(Config& new_config, uint32_t level){
-            config_ = new_config;
-            nss_ = config_.nss;
-            max_objectness_ = config_.max_objectness;
+      // detect
+      if (objectnessAlgorithm->computeSaliency(frame_, objectnessBoxes_))
+      {
+        rects_.header.frame_id = input_frame_from_msg;  // set header
+        // for(const cv::Vec4i& b : objectnessBoxes_){
+        for (int i = 0; i < std::min((int)objectnessBoxes_.size(), max_objectness_); i++)
+        {
+          // array b has (minX, minY, maxX, maxY)
+          cv::Vec4i& b = objectnessBoxes_.at(i);
+          opencv_apps::Rect rect_;
+          rect_.x = float(b[0] + b[2]) / 2.0;
+          rect_.y = float(b[1] + b[3]) / 2.0;
+          rect_.width = b[2] - b[0];
+          rect_.height = b[3] - b[1];
+          rects_.rects.push_back(rect_);
+          // draw rect in debug view
+          if (debug_view_)
+            cv::rectangle(debug_frame_, cv::Vec2i(b[0], b[1]), cv::Vec2i(b[2], b[3]), cv::Vec3i(0, 0, 255), 3);
         }
+        // publish
+        msg_pub_.publish(rects_);
+      }
 
-        void imageCallbackWithInfo(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info){
-            doWork(msg, cam_info->header.frame_id);
-        }
+      if (debug_view_)
+      {
+        cv::imshow(window_name_, debug_frame_);
+        int c = cv::waitKey(1);
+      }
+    }
+    catch (cv::Exception& e)
+    {
+      NODELET_ERROR("Image processing error: %s %s %s %i\n", e.err.c_str(), e.func.c_str(), e.file.c_str(), e.line);
+      NODELET_ERROR("Please check the training path set correctly at the same time\n");
+    }
+  }
 
-        void imageCallback(const sensor_msgs::ImageConstPtr& msg){
-            doWork(msg, msg->header.frame_id);
-        }
+  void subscribe()
+  {
+    NODELET_DEBUG("Subscribing to image topic.");
+    if (config_.use_camera_info)
+      cam_sub_ = it_->subscribeCamera("image", queue_size_, &ObjectnessNodelet::imageCallbackWithInfo, this);
+    else
+      img_sub_ = it_->subscribe("image", queue_size_, &ObjectnessNodelet::imageCallback, this);
+  }
 
-        void doWork(const sensor_msgs::ImageConstPtr& msg, const std::string& input_frame_from_msg){
-            try{
-                // declaration
-                std::vector<cv::Vec4i> objectnessBoxes_;
-                cv::Mat frame_, debug_frame_;
-                opencv_apps::RectArrayStamped rects_;
+  void unsubscribe()
+  {
+    NODELET_DEBUG("Unsubscribing from image topic.");
+    img_sub_.shutdown();
+    cam_sub_.shutdown();
+  }
 
-                // convert the image msg to cv object
-                if (msg->encoding == sensor_msgs::image_encodings::BGR8){
-                    frame_ = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8)->image;
-                }else if(msg->encoding == sensor_msgs::image_encodings::RGB8){
-                    frame_ = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::RGB8)->image;
-                    cv::cvtColor(frame_, frame_, CV_RGB2BGR);
-                }else if(msg->encoding == sensor_msgs::image_encodings::MONO8){
-                    frame_ = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::MONO8)->image;
-                }else{
-                    NODELET_ERROR_STREAM("Not supported image encoding: " << msg->encoding);
-                }
+  virtual void onInit()
+  {
+    Nodelet::onInit();
+    it_ = std::shared_ptr<image_transport::ImageTransport>(new image_transport::ImageTransport(*nh_));
+    pnh_->param("queue_size", queue_size_, 3);
+    pnh_->param("debug_view", debug_view_, false);
+    pnh_->getParam("training_path", training_path_);
 
-                if (debug_view_){
-                    cv::namedWindow(window_name_, cv::WINDOW_AUTOSIZE);
-                    debug_frame_ = frame_.clone();
-                }
+    window_name_ = "Objectness View";
 
-                // reconfigure
-                objectnessAlgorithm.dynamicCast<cv::saliency::ObjectnessBING>()->setNSS(nss_);
+    objectnessAlgorithm = cv::saliency::ObjectnessBING::create();  // support BING
 
-                // detect
-                if(objectnessAlgorithm->computeSaliency(frame_, objectnessBoxes_)){
-                    rects_.header.frame_id = input_frame_from_msg; // set header
-                    // for(const cv::Vec4i& b : objectnessBoxes_){
-                    for(int i=0; i<std::min((int)objectnessBoxes_.size(), max_objectness_); i++){
-                        // array b has (minX, minY, maxX, maxY)
-                        cv::Vec4i& b = objectnessBoxes_.at(i);
-                        opencv_apps::Rect rect_;
-                        rect_.x = float(b[0] + b[2]) / 2.0;
-                        rect_.y = float(b[1] + b[3]) / 2.0;
-                        rect_.width = b[2] - b[0];
-                        rect_.height = b[3] - b[1];
-                        rects_.rects.push_back(rect_);
-                        // draw rect in debug view
-                        if(debug_view_)
-                            cv::rectangle(debug_frame_, cv::Vec2i(b[0], b[1]), cv::Vec2i(b[2], b[3]), cv::Vec3i(0, 0, 255), 3);
-                    }
-                    // publish
-                    msg_pub_.publish(rects_);
-                }
+    NODELET_INFO_STREAM("BING Training path: " << training_path_);
+    objectnessAlgorithm.dynamicCast<cv::saliency::ObjectnessBING>()->setTrainingPath(training_path_);
 
-                if (debug_view_){
-                    cv::imshow(window_name_, debug_frame_);
-                    int c = cv::waitKey(1);
-                }
+    reconfigure_server_ = std::make_shared<dynamic_reconfigure::Server<Config> >(*pnh_);
+    typename dynamic_reconfigure::Server<Config>::CallbackType f =
+        std::bind(&ObjectnessNodelet::reconfigureCallback, this, std::placeholders::_1, std::placeholders::_2);
+    reconfigure_server_->setCallback(f);
 
-            }catch(cv::Exception& e){
-                NODELET_ERROR("Image processing error: %s %s %s %i\n", e.err.c_str(), e.func.c_str(), e.file.c_str(), e.line);
-                NODELET_ERROR("Please check the training path set correctly at the same time\n");
-            }
-        }
+    msg_pub_ = advertise<opencv_apps::RectArrayStamped>(*pnh_, "rect", 1);
 
-        void subscribe(){
-            NODELET_DEBUG("Subscribing to image topic.");
-            if (config_.use_camera_info)
-                cam_sub_ = it_->subscribeCamera("image", queue_size_, &ObjectnessNodelet::imageCallbackWithInfo, this);
-            else
-                img_sub_ = it_->subscribe("image", queue_size_, &ObjectnessNodelet::imageCallback, this);
-        }
-
-        void unsubscribe(){
-            NODELET_DEBUG("Unsubscribing from image topic.");
-            img_sub_.shutdown();
-            cam_sub_.shutdown();
-        }
-
-        virtual void onInit(){
-            Nodelet::onInit();
-            it_ = std::shared_ptr<image_transport::ImageTransport>(new image_transport::ImageTransport(*nh_));
-            pnh_->param("queue_size", queue_size_, 3);
-            pnh_->param("debug_view", debug_view_, false);
-            pnh_->getParam("training_path", training_path_);
-
-            window_name_ = "Objectness View";
-
-            objectnessAlgorithm = cv::saliency::ObjectnessBING::create(); // support BING
-
-            NODELET_INFO_STREAM("BING Training path: " << training_path_);
-            objectnessAlgorithm.dynamicCast<cv::saliency::ObjectnessBING>()->setTrainingPath(training_path_);
-
-            reconfigure_server_ = std::make_shared<dynamic_reconfigure::Server<Config> >(*pnh_);
-            typename dynamic_reconfigure::Server<Config>::CallbackType f =
-                std::bind(&ObjectnessNodelet::reconfigureCallback, this, std::placeholders::_1, std::placeholders::_2);
-            reconfigure_server_->setCallback(f);
-
-            msg_pub_ = advertise<opencv_apps::RectArrayStamped>(*pnh_, "rect", 1);
-
-            onInitPostProcess();
-        }
-    };
-}
+    onInitPostProcess();
+  }
+};
+}  // namespace opencv_apps
 
 #include <pluginlib/class_list_macros.h>
 PLUGINLIB_EXPORT_CLASS(opencv_apps::ObjectnessNodelet, nodelet::Nodelet);
