24aee47ae435fa284ffddb5c1539e9fa4d835ac5
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2025-02-14 17:43:23 +0900

    add ROS-O test

--- .travis.sh
+++ .travis.sh
@@ -26,7 +26,18 @@ function setup {
     travis_time_start setup.before_install
     #before_install:
     # Install ROS
-    sudo sh -c "echo \"deb http://packages.ros.org/ros-shadow-fixed/ubuntu `lsb_release -cs` main\" > /etc/apt/sources.list.d/ros-latest.list"
+    if [[ "$ROS_DISTRO" ==  "one" ]]; then
+        UBUNTU_DISTRO=$(echo "$DOCKER_IMAGE" | cut -d':' -f2)
+        echo "deb [trusted=yes] https://raw.githubusercontent.com/k-okada/ros-o-builder/$UBUNTU_DISTRO-one-unstable/repository/ ./" | sudo tee /etc/apt/sources.list.d/ros-o-builder.list
+        DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC sudo -E apt install -y software-properties-common
+        [[ "$UBUNTU_DISTRO" ==  "noble" ]] && sudo -E add-apt-repository -y ppa:v-launchpad-jochen-sprickerhof-de/ros
+        [[ "$UBUNTU_DISTRO" ==  "jammy" ]] && sudo -E add-apt-repository -y ppa:k-okada/python3-catkin-tools
+        DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC sudo -E apt install -y python3-rosdep2
+        echo "yaml https://raw.githubusercontent.com/k-okada/ros-o-builder/$UBUNTU_DISTRO-one-unstable/repository/local.yaml debian" | sudo tee /etc/ros/rosdep/sources.list.d/1-ros-o-builder.list
+rosdep update
+    else
+        sudo sh -c "echo \"deb http://packages.ros.org/ros-shadow-fixed/ubuntu `lsb_release -cs` main\" > /etc/apt/sources.list.d/ros-latest.list"
+    fi
     wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
     # Setup EoL repository
     if [[ "$ROS_DISTRO" ==  "hydro" || "$ROS_DISTRO" ==  "jade" || "$ROS_DISTRO" ==  "lunar" ]]; then
@@ -41,15 +52,33 @@ function setup {
     fi
     ###
     # Install ROS
-    if [[ "$ROS_DISTRO" ==  "noetic" ]]; then
+    if [[ "$ROS_DISTRO" ==  "one" ]]; then
+        if [[ "$UBUNTU_DISTRO" == "jammy" ]]; then
+            sudo apt-get install -y -q python3-catkin-pkg python3-wstool python3-rosinstall-generator python3-osrf-pycommon
+            sudo apt-get install -y -q python3-catkin-tools
+        else
+            sudo apt-get install -y -q python3-pip python3-zombie-imp
+            pip3 install --user --break-system-packages vcstool
+            pip3 install --user --break-system-packages rosinstall-generator
+            export PATH="$PATH:~/.local/bin"
+            sudo apt-get install -y -q python3-catkin-pkg python3-osrf-pycommon
+            sudo apt-get install -y -q catkin-tools
+        fi
+    elif [[ "$ROS_DISTRO" ==  "noetic" ]]; then
         sudo apt-get install -y -q python3-catkin-pkg python3-catkin-tools python3-rosdep python3-wstool python3-rosinstall-generator python3-osrf-pycommon
     else
         sudo apt-get install -y -q python-catkin-pkg python-catkin-tools python-rosdep python-wstool python-rosinstall-generator
     fi
-    sudo apt-get install -y -q ros-$ROS_DISTRO-catkin
+    if [[ "$ROS_DISTRO" ==  "one" ]]; then
+        sudo apt-get install -y -q catkin
+        sudo apt-get install -y -q ros-$ROS_DISTRO-rosbash ros-$ROS_DISTRO-ros-environment
+    else
+        sudo apt-get install -y -q ros-$ROS_DISTRO-catkin
+    fi
+    export ROS_DISTRO=$_ROS_DISTRO
     source /opt/ros/$ROS_DISTRO/setup.bash
     # Setup for rosdep
-    sudo rosdep init
+    [ -e /etc/ros/rosdep/sources.list.d ] || sudo rosdep init
     # use snapshot of rosdep list
     # https://github.com/ros/rosdistro/pull/31570#issuecomment-1000497517
     if [[ "$ROS_DISTRO" =~ "hydro"|"indigo"|"jade"|"kinetic"|"lunar" ]]; then
@@ -78,14 +107,16 @@ function setup {
     #before_script:
     # source dependencies: install using wstool.
     cd ~/catkin_ws/src
-    wstool init
+    wstool init || echo "OK"
     #if [[ -f $ROSINSTALL_FILE ]] ; then wstool merge $ROSINSTALL_FILE ; fi
     if [ "$OPENCV_VERSION" == 3 ]; then rosinstall_generator image_pipeline --upstream >> .rosinstall.opencv3; fi # need to recompile image_proc
     if [ "$OPENCV_VERSION" == 3 ]; then rosinstall_generator compressed_image_transport --upstream >> .rosinstall.opencv3; fi # need to recompile compressed_image_transport
     if [ "$OPENCV_VERSION" == 3 ]; then rosinstall_generator vision_opencv --upstream >> .rosinstall.opencv3; fi # need to recompile visoin_opencv
-    if [ "$OPENCV_VERSION" == 3 ]; then wstool merge .rosinstall.opencv3; fi # need to recompile visoin_opencv
-    wstool up
-    wstool info
+    if [ "$OPENCV_VERSION" == 3 ]; then wstool merge .rosinstall.opencv3 || [ ! -s .rosinstall.opencv3 ] || vcs import . < .rosinstall.opencv3 ; fi # need to recompile visoin_opencv
+    git config --global --add safe.directory $CI_SOURCE_PATH
+    git config --global --add safe.directory ~/catkin_ws/src || echo "OK"
+    wstool up || vcs pull
+    wstool info || vcs branch
     if [ "$OPENCV_VERSION" == 3 ]; then  sed -i 's@libopencv-dev@opencv3@' */*/package.xml ; fi
 
 
@@ -96,6 +127,7 @@ function setup {
 }
 
 function build {
+    export ROS_DISTRO=$_ROS_DISTRO
     travis_time_start build.script
     source /opt/ros/$ROS_DISTRO/setup.bash
     cd ~/catkin_ws
@@ -104,6 +136,7 @@ function build {
 }
 
 function run_test {
+    export ROS_DISTRO=$_ROS_DISTRO
     travis_time_start run_test.script
     source /opt/ros/$ROS_DISTRO/setup.bash
     cd ~/catkin_ws
@@ -113,6 +146,7 @@ function run_test {
 }
 
 function build_install {
+    export ROS_DISTRO=$_ROS_DISTRO
     travis_time_start build_install.script
     source /opt/ros/$ROS_DISTRO/setup.bash
     cd ~/catkin_ws
@@ -122,9 +156,12 @@ function build_install {
     travis_time_end
 }
 
+# setup.bash override ROS_DISTRO...
+export _ROS_DISTRO=$ROS_DISTRO
+
 travis_time_start apt.before_install
 apt-get -y -qq update || if [ $? -eq 100 ]; then sed -i 's/archive.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list; apt-get -y -qq update; fi
-apt-get install -y -q wget sudo lsb-release gnupg # for docker
+apt-get install -y -q wget sudo lsb-release gnupg ca-certificates git # for docker
 # set DEBIAN_FRONTEND=noninteractive
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 travis_time_end
@@ -132,10 +169,12 @@ travis_time_end
 if [ "$TEST" == "catkin_lint" ]; then
 
     travis_time_start catkin_lint.script
-    apt-get install -y -q python-pip
-    pip install catkin_lint rosdep
+    apt-get install -y -q python3-pip
+    # See https://github.com/ros-perception/opencv_apps/pull/143
+    # In catkin_lint > 1.6.18, cmake_minimum_required >= 2.8.12
+    sudo pip3 install catkin_lint==1.6.18 rosdep
     rosdep init
-    rosdep update
+    rosdep update --include-eol-distros
     travis_time_end
     ROS_DISTRO=melodic catkin_lint --resolve-env --strict $CI_SOURCE_PATH
 
